f0ed2bfd7ff06ad7efa56003bec86e441888f99f
Alex <adrysdale@protonmail.com>
2023-05-17 11:58:54 +0100

    Adds a func to check/setup tesseract
    If on a windows system, will assume tesseract is in Program files
    on other system does nothing.
    If on a non-windows or linux system, will return a warning that can
    be ignored if pytesseract is in the path.

--- usseg/Refined_anon_2_html.py
+++ usseg/Refined_anon_2_html.py
@@ -5,6 +5,7 @@
 # Python imports
 import os
 from os import walk
+import logging
 import sys
 import re
 from PIL import Image
@@ -25,10 +26,39 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(BASE_DIR)
 from usseg import General_functions
 
-#pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
+def setup_tesseract():
+    """Checks tesseract is set up appropriately
+
+    Currently does nothing on a linux system and sets the
+    pytesseract.pytesseract.tesseract_cmd to "C:/Program Files/Tesseract-OCR/tesseract.exe"
+    for Windows and Cygwin systems.
+
+    Any other system (including MACOS) a warning is displayed and nothing is done.
+    It is expected, for non-Windows/Cygwin systems that tesseract is available in the PATH.
+
+    If this is not the desired behaviour, please specify tesseract_cmd after running this
+    script.
+
+    Returns:
+        tesseract_version (str) : Returns the tesseract version installed.
+    """
+    if sys.platform.startswith('linux'):
+        pass
+    elif sys.platform.startswith('win32'):
+        pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
+    elif sys.platform.startswith('cygwin'):
+        pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
+    else:
+        logging.warning(
+            f"Platform {sys.platform} is not recognised.\n"
+            "Please ensure that you added pytesseract to your system's path."
+        )
+
+    return pytesseract.get_tesseract_version()
 
 
 def segment():
+
     # filenames = next(walk('Scans/'), (None, None, []))[2]  # [] if no file
     # text_file = open("sample3_file_list_test.txt", "r")
     # filenames = text_file.read().replace('\'','').replace('[','').replace(']','').replace(' ','').split(',')
