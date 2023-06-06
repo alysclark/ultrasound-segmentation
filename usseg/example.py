194415134c35b037ccb8d90f59290921a3411fac
Alex <adrysdale@protonmail.com>
2023-06-06 16:53:07 +0100

    Switched to test image

--- usseg/example.py
+++ usseg/example.py
@@ -2,9 +2,6 @@
 import os
 import sys
 
-# Module imports
-import toml
-
 # Local imports
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(BASE_DIR)
@@ -13,10 +10,8 @@ import usseg
 from PIL import Image
 import cv2
 import matplotlib.pyplot as plt
-PIL_img = Image.open('E:\\us-data-anon\\0000\\IHE_PDI\\00003511\\AA3A43F2\\AAD8766D\\0000371E\\EE511F45.JPG')
-cv2_img = cv2.imread('E:\\us-data-anon\\0000\\IHE_PDI\\00003511\\AA3A43F2\\AAD8766D\\0000371E\\EE511F45.JPG')
 
-def main(root_dir):
+def main(PIL_img, cv2_img):
     """Main function that performs all of the segmentation on a root directory"""
     # Checks and sets up the tesseract environment
     usseg.setup_tesseract()
@@ -27,7 +22,8 @@ def main(root_dir):
     plt.figure()
     plt.plot(raw_signal[0], raw_signal[1], "-")
 
-i=0
 if __name__ == "__main__":
-    root_dir = toml.load("config.toml")["root_dir"]
-    main(root_dir)
+    filename = os.path.abspath("Lt_test_image.png")
+    PIL_img = Image.open(filename)
+    cv2_img = cv2.imread(filename)
+    main(PIL_img, cv2_img)
