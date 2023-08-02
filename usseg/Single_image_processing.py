804f6082dea194601609840a73220f973d8d69be
Alex <adrysdale@protonmail.com>
2023-08-02 16:16:14 +0100

    Removed try and except blocks

--- usseg/Single_image_processing.py
+++ usseg/Single_image_processing.py
@@ -30,162 +30,104 @@ def data_from_image(PIL_img,cv2_img):
         df (pandas dataframe) : Dataframe of extracted text.
         XYdata (list) : X and Y coordinates of the extracted segmentation.
     """
-    try:  
-        PIL_image_RGB = PIL_img.convert("RGB")  # We need RGB, so convert here. with PIL
+    # Extracts yellow text from image
+    PIL_image_RGB = PIL_img.convert("RGB")  # We need RGB, so convert here. with PIL
+    COL = General_functions.Colour_extract(PIL_image_RGB, [255, 255, 100], 80, 80)
+    text_extract_failed, df = General_functions.Text_from_greyscale(cv2_img, COL)
+    # Failure not really relavent to the rest of the segmenation so just logged as 
+    # a warning for the end user.
+    if text_extract_failed:
+        logger.warning("Couldn't extract text from image. Continuing...")
+    else:
+        logger.info("Completed colour extraction.")
 
-        COL = General_functions.Colour_extract(PIL_image_RGB, [255, 255, 100], 80, 80)
-        logger.info("Completed colour extraction")
+    # No error handling for initial segmentation as impossible to complete segmentation
+    # without segmenation mask.
+    segmentation_mask, Xmin, Xmax, Ymin, Ymax = General_functions.Initial_segmentation(
+        input_image_obj=PIL_image_RGB
+    )
 
-        Fail, df = General_functions.Text_from_greyscale(cv2_img, COL)
-    except Exception:  # flat fail on 1
-        traceback.print_exc()  # prints the error message and traceback
-        logger.error("Failed text extraction")
-        Fail = 0
-        pass
+    # Gets ROIS
+    Left_dimensions, Right_dimensions = General_functions.Define_end_ROIs(
+        segmentation_mask, Xmin, Xmax, Ymin, Ymax
+    )
 
-    try:  # Try initial segmentation
-        segmentation_mask, Xmin, Xmax, Ymin, Ymax = General_functions.Initial_segmentation(
-            input_image_obj=PIL_image_RGB
-        )
-    except Exception:  # flat fail on 1
-        logger.error("Failed Initial segmentation")
-        Fail = Fail + 1
-        pass
+    # Search for ticks and labels
+    (
+        Cs,
+        ROIAX,
+        CenPoints,
+        onY,
+        BCs,
+        TYLshift,
+        thresholded_image,
+        Side,
+        Left_dimensions,
+        Right_dimensions,
+        ROI2,
+        ROI3,
+    ) = General_functions.Search_for_ticks(
+        cv2_img, "Left", Left_dimensions, Right_dimensions
+    )
+    ROIAX, Lnumber, Lpositions, ROIL = General_functions.Search_for_labels(
+        Cs,
+        ROIAX,
+        CenPoints,
+        onY,
+        BCs,
+        TYLshift,
+        Side,
+        Left_dimensions,
+        Right_dimensions,
+        cv2_img,
+        ROI2,
+        ROI3,
+    )
 
-    try:  # define end ROIs
-        Left_dimensions, Right_dimensions = General_functions.Define_end_ROIs(
-            segmentation_mask, Xmin, Xmax, Ymin, Ymax
-        )
-    except Exception:
-        logger.error("Failed Defining ROI")
-        Fail = Fail + 1
-        pass
+    (
+        Cs,
+        ROIAX,
+        CenPoints,
+        onY,
+        BCs,
+        TYLshift,
+        thresholded_image,
+        Side,
+        Left_dimensions,
+        Right_dimensions,
+        ROI2,
+        ROI3,
+    ) = General_functions.Search_for_ticks(
+        cv2_img, "Right", Left_dimensions, Right_dimensions
+    )
+    ROIAX, Rnumber, Rpositions, ROIR = General_functions.Search_for_labels(
+        Cs,
+        ROIAX,
+        CenPoints,
+        onY,
+        BCs,
+        TYLshift,
+        Side,
+        Left_dimensions,
+        Right_dimensions,
+        cv2_img,
+        ROI2,
+        ROI3,
+    )
 
-    try:
-        Waveform_dimensions = [Xmin, Xmax, Ymin, Ymax]
-    except Exception:
-        logger.error("Failed Waveform dimensions")
-        Fail = Fail + 1
-        pass
+    (
+        refined_segmentation_mask, top_curve_mask, top_curve_coords
+    ) = General_functions.Segment_refinement(
+        cv2_img, Xmin, Xmax, Ymin, Ymax
+    )
 
-    try:  # Search for ticks and labels
-        (
-            Cs,
-            ROIAX,
-            CenPoints,
-            onY,
-            BCs,
-            TYLshift,
-            thresholded_image,
-            Side,
-            Left_dimensions,
-            Right_dimensions,
-            ROI2,
-            ROI3,
-        ) = General_functions.Search_for_ticks(
-            cv2_img, "Left", Left_dimensions, Right_dimensions
-        )
-        ROIAX, Lnumber, Lpositions, ROIL = General_functions.Search_for_labels(
-            Cs,
-            ROIAX,
-            CenPoints,
-            onY,
-            BCs,
-            TYLshift,
-            Side,
-            Left_dimensions,
-            Right_dimensions,
-            cv2_img,
-            ROI2,
-            ROI3,
-        )
+    # Gets the segmentation
+    Xplot, Yplot, Ynought = General_functions.Plot_Digitized_data(
+        Rnumber, Rpositions, Lnumber, Lpositions, top_curve_coords,
+    )
 
-        (
-            Cs,
-            ROIAX,
-            CenPoints,
-            onY,
-            BCs,
-            TYLshift,
-            thresholded_image,
-            Side,
-            Left_dimensions,
-            Right_dimensions,
-            ROI2,
-            ROI3,
-        ) = General_functions.Search_for_ticks(
-            cv2_img, "Right", Left_dimensions, Right_dimensions
-        )
-        ROIAX, Rnumber, Rpositions, ROIR = General_functions.Search_for_labels(
-            Cs,
-            ROIAX,
-            CenPoints,
-            onY,
-            BCs,
-            TYLshift,
-            Side,
-            Left_dimensions,
-            Right_dimensions,
-            cv2_img,
-            ROI2,
-            ROI3,
-        )
-    except Exception:
-        traceback.print_exc()  # prints the error message and traceback
-        logger.error("Failed Axes search")
-        
-        Fail = Fail + 1
-        pass
-
-    try:  # Refine segmentation
-        (
-            refined_segmentation_mask, top_curve_mask, top_curve_coords
-        ) = General_functions.Segment_refinement(
-            cv2_img, Xmin, Xmax, Ymin, Ymax
-        )
-    except Exception:
-        traceback.print_exc()  # prints the error message and traceback
-        logger.error("Failed Segment refinement")
-        Fail = Fail + 1
-        pass
-
-    try: 
-        Xplot, Yplot, Ynought = General_functions.Plot_Digitized_data(
-            Rnumber, Rpositions, Lnumber, Lpositions, top_curve_coords,
-        )
-
-    except Exception:
-        logger.error("Failed Digitization")
-        traceback.print_exc()
-        Fail = Fail + 1
-        pass
-
-    try:
+    if not text_extract_failed:
         df = General_functions.Plot_correction(Xplot, Yplot, df)
-    except Exception:
-        traceback.print_exc()
-        logger.error("Failed correction")
-        pass
-    to_del = [
-        "df",
-        "image_name",
-        "Xmax",
-        "Xmin",
-        "Ymax",
-        "Ymin",
-        "Rnumber",
-        "Rpositions",
-        "Lnumber",
-        "Lpositions",
-        "Left_dimensions",
-        "Right_dimensions",
-        "segmentation_mask",
-    ]
-    for i in to_del:
-        try:
-            exec("del %s" % i)
-        except Exception:
-            pass
 
     plt.close("all")
     XYdata = [Xplot,Yplot]
