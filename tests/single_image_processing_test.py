5b89d82725f78760bc771a7a238d6924662901de
Alex <adrysdale@protonmail.com>
2023-08-02 16:16:27 +0100

    Updated tests

--- tests/single_image_processing_test.py
+++ tests/single_image_processing_test.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import logging
+import pytest
 
 # Module imports
 import numpy as np
@@ -24,8 +25,13 @@ def test_data_from_image():
 
     PIL_image = Image.open(img_path)
     cv2_image = np.array(PIL_image)
+    logger.info(f"Loaded image with shape {cv2_image.shape} and type {cv2_image.dtype}")
 
     df, (xdata, ydata) = data_from_image(PIL_image, cv2_image)
+
+    # Makes sure that the lists aren't empty
+    assert xdata
+    assert ydata
 
     logger.info(f"Extracted the following text from {img_path}:\n{df}")
 
@@ -47,7 +53,21 @@ def test_data_from_image():
     for i, val in enumerate(values):
         assert np.abs(df.at[i, "Value"] - val) < tol * val
 
+def test_failures():
+    """Test that the correct fail responses are being raised."""
+    
+    # Failed extraction
+    cv2_img = np.random.default_rng().integers(0, 256, size=(100, 100, 3), dtype=np.uint8)
+    PIL_img = Image.fromarray(cv2_img)
+
+    with pytest.raises(ValueError) as exc_info:
+        data_from_image(PIL_img, cv2_img)
+
+    exc_raised = str(exc_info.value)
+    assert exc_raised == "zero-size array to reduction operation minimum which has no identity"
+
 
 if __name__ == "__main__":
     test_data_from_image()
+    test_failures()
     logger.info(f"{__file__} tests have passed!")
