cc2ce1364185d911ebcb6f48228197debcf35724
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-05-18 11:08:26 +1200

    Rename Refined_anon_2_html to refined_anon_2_html

--- src/usseg/__init__.py
+++ src/usseg/__init__.py
@@ -3,7 +3,7 @@ from importlib.metadata import version, PackageNotFoundError
 from usseg import general_functions
 from usseg.organise_files import get_likely_us
 from usseg.Single_image_processing import data_from_image
-from usseg.Refined_anon_2_html import setup_tesseract, segment
+from usseg.refined_anon_2_html import setup_tesseract, segment
 from usseg.visualisation_html import generate_html_from_pkl, generate_html
 from usseg.main import main
 # Replace this with loading from configuration.
